76cf9dcf94531fa2a5d7684573b494fd6af11c5d
vaibhavw12 <vaibhavwable2000@gmail.com>
2023-11-27 09:21:30 +0530

    routes are added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div>
-        <h2 className='text-red-500'>App</h2>
-    </div>
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Home></Home>}></Route>
+            <Route path='/about' element={<About></About>}></Route>
+            <Route path='/sign-in' element={<SignIn></SignIn>}></Route>
+            <Route path='/sign-up' element={<SignUp></SignUp>}></Route>
+            <Route path='/profile' element={<Profile></Profile>}></Route>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
