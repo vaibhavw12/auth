b005e2d334b7fee4ebf4ed5951591a0591b0fc8f
vaibhavw12 <vaibhavwable2000@gmail.com>
2023-11-30 08:46:02 +0530

    sign in page is created in client a

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,51 +1,10 @@
-import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
-import axios from 'axios'
+import React from 'react'
 
 export default function Home() {
 
-  const [formData, setFormData] = useState({})
-  const [error, setError] = useState('')
-  const [loading, setLoading] = useState(false)
-
-  const handleChange = (e)=>{
-    setFormData({...formData, [e.target.id] : e.target.value})
-    setError('')
-  }
-  const handelSubmit = async(e)=>{
-    e.preventDefault()
-    setLoading(true)
-    const res =  await axios.post('http://localhost:3000/api/auth/signup',formData)
-    if(res.data.status === 'FAILED'){
-      setError('something went wrong')
-    }else{
-      setError('')
-    }
-    setLoading(false)
-  }
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form onSubmit={handelSubmit} className='flex flex-col gap-4'>
-        <input onChange={handleChange} type='text' placeholder='username' id='username' 
-        className='bg-slate-100 p-3 rounded-lg'>
-        </input>
-        <input onChange={handleChange} type='email' placeholder='email' id='email' 
-        className='bg-slate-100 p-3 rounded-lg'>
-        </input>
-        <input onChange={handleChange} type='password' placeholder='password' id='password' 
-        className='bg-slate-100 p-3 rounded-lg'>
-        </input>
-        <button className='bg-slate-700 text-white p-3 rounded-lg
-        uppercase hover:opacity-95 disabled:opacity-80'>{loading ? <span>loading...</span> : <span>Sign Up</span>}</button>
-      </form>
-      <div className='flex gap-3 mt-5'>
-        <p>Already have an account ?</p>
-        <Link to='/sign-in'>
-          <span className='text-blue-500'>Sign in</span>
-        </Link>
-      </div>
-      <p className='text-red-700 mt-2'>{error}</p>
+    <div className=''>
+      home
     </div>
   )
 }
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import axios from 'axios'
 
-export default function Signin() {
+export default function SignIn() {
+
+  const navigate = useNavigate()
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const handleChange = (e)=>{
+    setFormData({...formData, [e.target.id] : e.target.value})
+    setError('')
+  }
+  const handelSubmit = async(e)=>{
+    e.preventDefault()
+    setLoading(true)
+    const res =  await axios.post('http://localhost:3000/api/auth/signin',formData)
+    console.log(res.data)
+    navigate('/')
+    if(res.data.status === 'FAILED'){
+      setError('something went wrong')
+    }else{
+      setError('')
+    }
+    setLoading(false)
+  }
   return (
-    <div>Signin</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form onSubmit={handelSubmit} className='flex flex-col gap-4'>
+        <input onChange={handleChange} type='email' placeholder='email' id='email' 
+        className='bg-slate-100 p-3 rounded-lg'>
+        </input>
+        <input onChange={handleChange} type='password' placeholder='password' id='password' 
+        className='bg-slate-100 p-3 rounded-lg'>
+        </input>
+        <button className='bg-slate-700 text-white p-3 rounded-lg
+        uppercase hover:opacity-95 disabled:opacity-80'>{loading ? <span>loading...</span> : <span>Sign In</span>}</button>
+      </form>
+      <div className='flex gap-3 mt-5'>
+        <p>Dont't have an account ?</p>
+        <Link to='/sign-up'>
+          <span className='text-blue-500'>Sign Up</span>
+        </Link>
+      </div>
+      <p className='text-red-700 mt-2'>{error}</p>
+    </div>
   )
 }
